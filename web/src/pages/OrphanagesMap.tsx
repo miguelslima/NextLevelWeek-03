357a8bc6afd93803a50e6e598ff1d156def56921
Miguel <miguelsousalima@hotmail.com>
2020-10-13 18:04:47 -0300

    Integracao Maps Leaflet

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiPlus } from "react-icons/fi";
+import { Map, TileLayer } from "react-leaflet";
+
+import 'leaflet/dist/leaflet.css'
+
+import mapMarkerImg from "../images/map-marker.svg";
+
+import "../styles/pages/orphanages-map.css";
 
 function OrphanagesMap() {
   return (
-    <h1>Orfanatos</h1>
-  )
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="" />
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita ;)</p>
+        </header>
+
+        <footer>
+          <strong>Mossoró</strong>
+          <span>Rio Grande do Norte</span>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-5.1766991, -37.3720384]}
+        zoom={15}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </Map>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#fff" />
+      </Link>
+    </div>
+  );
 }
 
 export default OrphanagesMap;
