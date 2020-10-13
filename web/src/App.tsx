9f90ed77c9538551ba41e8b429a08466de8a4f14
Miguel <miguelsousalima@hotmail.com>
2020-10-12 23:19:29 -0300

    Primeiro Dia - Comeco landing page

---
+++ web/src/App.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import "./styles/global.css";
+import "./styles/pages/landing.css";
+
+import logoImg from "./images/logo.svg";
+
+function App() {
+  return (
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Logo Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Mossoró</strong>
+          <span>Rio Grande do Norte</span>
+        </div>
+          <a href="" className="enter-app">
+            >
+          </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
