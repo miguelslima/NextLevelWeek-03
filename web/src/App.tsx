1db041b7e9cb4b85e7278fe43d8afc8449dba850
Miguel <miguelsousalima@hotmail.com>
2020-10-13 17:33:33 -0300

    Pages Landing e Orphanages e config Rotas

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,31 +1,8 @@
 import React from "react";
-
-import "./styles/global.css";
-import "./styles/pages/landing.css";
-
-import logoImg from "./images/logo.svg";
+import Routes from "./routes";
 
 function App() {
-  return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Logo Happy" />
-
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-        </main>
-
-        <div className="location">
-          <strong>Mossoró</strong>
-          <span>Rio Grande do Norte</span>
-        </div>
-          <a href="" className="enter-app">
-            >
-          </a>
-      </div>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
