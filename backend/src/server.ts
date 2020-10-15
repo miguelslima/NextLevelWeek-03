7391e37d3bb2cf02c2ccc4629e1a5f4199445b13
Miguel <miguelsousalima@hotmail.com>
2020-10-14 22:59:01 -0300

    Ajustes MVC - Model e Controller Orphanage

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,12 @@
 import express from "express";
+import "./database/connection";
+
+import routes from './routes'
 
 const app = express();
+
 app.use(express.json());
+app.use(routes);
 
-app.get("/users", (request, response) => {
-
-  return response.json({ nome: "Miguel" });
-});
 
 app.listen(3333);
