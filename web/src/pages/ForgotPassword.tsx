7de80313a4c476beeb243150707ae12941c6230e
Miguel <miguelsousalima@hotmail.com>
2020-10-21 19:36:59 -0300

    Ajustes pages forgotpassword

--- web/src/pages/ForgotPassword.tsx
+++ web/src/pages/ForgotPassword.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { FiArrowLeft } from "react-icons/fi";
 import { Link } from "react-router-dom";
@@ -9,29 +9,69 @@ import "../styles/global.css";
 import "../styles/pages/forgotpassword.css";
 
 function ForgotPassword() {
+  const [emailPreenchido, setEmailPreenchido] = useState(false);
+
+  function handleTest() {
+    setEmailPreenchido(!emailPreenchido);
+  }
+
   return (
     <div id="container">
-      <div id="page-login">
-        <div className="content-wrapper">
-          <img src={logoImg} alt="Logo Happy" />
+      {emailPreenchido ? (
+        <>
+          <div id="page-login">
+            <div className="content-wrapper">
+              <img src={logoImg} alt="Logo Happy" />
 
-          <div className="local">
-            <strong>Mossoró</strong>
-            <span>Rio Grande do Norte</span>
+              <div className="local">
+                <strong>Mossoró</strong>
+                <span>Rio Grande do Norte</span>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div id="login-wrapper">
-        <Link to="/" className="back-home">
-          <FiArrowLeft size={48} color="#15C3D6" />
-        </Link>
+          <div id="forgot-wrapper">
+            <Link to="/login" className="back-home">
+              <FiArrowLeft size={48} color="#15C3D6" />
+            </Link>
 
-        <h1>Redefinição de senha</h1>
-        <span>Sua redefinição de senha será enviada para o e-mail cadastrado</span>
-        <p>E-mail</p>
-        <input type="text" />
-        <button>Entrar</button>
-      </div>
+            <h1>Redefinição de senha</h1>
+            <span>
+              Escolha uma nova senha para você acessar o dashboard do Happy
+            </span>
+            <p>Nova senha</p>
+            <input type="password" />
+            <p>Repetir senha</p>
+            <input type="password" />
+            <button onClick={handleTest}>Entrar</button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div id="page-login">
+            <div className="content-wrapper">
+              <img src={logoImg} alt="Logo Happy" />
+
+              <div className="local">
+                <strong>Mossoró</strong>
+                <span>Rio Grande do Norte</span>
+              </div>
+            </div>
+          </div>
+          <div id="forgot-wrapper">
+            <Link to="/login" className="back-home">
+              <FiArrowLeft size={48} color="#15C3D6" />
+            </Link>
+
+            <h1>Esqueci a senha</h1>
+            <span>
+              Sua redefinição de senha será enviada para o e-mail cadastrado
+            </span>
+            <p>E-mail</p>
+            <input type="text" />
+            <button onClick={handleTest}>Entrar</button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
